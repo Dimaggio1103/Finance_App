d110d412a39089dfd9214d4d78da4b19a37fd991
robert <robert@profitboost.com>
2020-03-02 13:17:25 -0700

    version .0002

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: 'Tpk_8f0660d9f3f2433f8a6a1069b939bf2c',
                                   endpoint: 'https://sandbox.iexapis.com/v1')
